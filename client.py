18af8323599f26600e526641e896ef9573268a1e
shwsun <shwsun@bu.edu>
2015-10-08 04:01:40 -0400

    update

---
+++ client.py
@@ -0,0 +1,131 @@
+
+
+import socket
+import configparser
+import os
+import sys
+from datetime import datetime
+
+HOST = '128.197.11.36'
+PORT = 58909  # test port
+RECEIVE_BYTES = 4096
+
+
+from config import NUM_PROBES
+from config import NUM_TRIALS
+from config import SERVER_DELAYS
+from config import MEASUREMENTS
+
+
+class Statistics(object):
+    def __init__(self, send_time, recv_time, message_size):
+        self.send_time = send_time
+        self.recv_time = recv_time
+        self.message_size = message_size
+
+    def round_trip_time(self):
+        return (self.recv_time - self.send_time).total_seconds()
+
+    def throughput(self):
+        return self.message_size / self.round_trip_time()
+
+
+
+class Experiment(object):
+    def __init__(self, host, port, measurement_type, num_probes, message_size, delay):
+        self.host, self.port = host, port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.measurement_type = measurement_type
+        self.max_probes = num_probes
+        self.message_size = message_size
+        self.delay = delay
+        self.stats = []
+
+    def __enter__(self):
+        self.sock.connect((self.host, self.port))
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.sock.close()
+
+    def check_for_error(self, data):
+        if data.startswith(b'404 ERROR'):
+            raise ServerException(data[11:])
+
+    def request_bytes(self, *args, suffix=b'', **kw):
+        if suffix:
+            suffix = b' ' + suffix
+        return ' '.join(args).encode('utf8') + suffix + b'\n'
+
+    def run(self):
+        self.send_setup_connection()
+        self.send_probes()
+        self.send_termination()
+
+    def send_and_recv(self, message):
+        self.sock.send(message)
+        result = self.sock.recv(RECEIVE_BYTES)
+        return result
+
+    def send_probes(self):
+        for n in range(1, self.max_probes + 1):
+            data = self.request_bytes('m', str(n),
+                                      suffix=b'.' * self.message_size)
+            time_sent = datetime.now()
+            self.sock.send(data)
+            received = self.sock.recv(RECEIVE_BYTES)
+            while len(received) < self.message_size:
+                received += self.sock.recv(RECEIVE_BYTES)
+            time_received = datetime.now()
+            self.check_for_error(received)
+            result = Statistics(time_sent, time_received, self.message_size)
+            self.stats.append(result)
+
+    def send_setup_connection(self):
+        data = self.request_bytes('s', self.measurement_type,
+                                  str(self.max_probes), str(self.message_size),
+                                  str(self.delay))
+        result = self.send_and_recv(data)
+        self.check_for_error(result)
+
+    def send_termination(self):
+        result = self.send_and_recv(b't \n')
+        self.check_for_error(result)
+
+def main(host, port):
+    results = {}
+    for d in SERVER_DELAYS:
+        results[d] = {}
+        for m, sizes in MEASUREMENTS.items():
+            results[d][m] = {}
+            for s in sizes:
+                results[d][m][s] = {}
+                for n in range(NUM_TRIALS):
+                    with Experiment(host, int(port), m, NUM_PROBES, s, d) as e:
+                        e.run()
+                        if m == 'rtt':
+                            data = [x.round_trip_time() for x in e.stats]
+                        else:
+                            data = [x.throughput() for x in e.stats]
+                        results[d][m][s][n] = data
+    write_results(results)
+
+
+def write_results(results, f=sys.stdout):
+    f.write('# written at {}\n'.format(datetime.now().isoformat()))
+    f.write('# delay measurement_type msg_size trial_num probe_num value\n')
+    for delay in SERVER_DELAYS:
+        for m, sizes in MEASUREMENTS.items():
+            for size in sizes:
+                for trial_num in range(NUM_TRIALS):
+                    values = results[delay][m][size][trial_num]
+                    assert len(values) == NUM_PROBES
+                    for probe_num in range(NUM_PROBES):
+                        f.write(' '.join(('{}' for n in range(6)))
+                                .format(delay, m, size, trial_num, probe_num,
+                                        values[probe_num]))
+                        f.write('\n')
+
+
+if __name__ == '__main__':
+    main(HOST, PORT)
--- server.py
+++ server.py
@@ -22,42 +22,27 @@ class MeasurementProtocolHandler(socketserver.StreamRequestHandler):
             self.message_size = int(message_size)
             self.delay = float(delay)
         except ValueError:
-            #logging.debug('responding with error: could not get int')
             self.respond_error(ERR_SETUP)
             self.still_processing = False
             return
         if measurement_type not in MEASUREMENT_TYPES:
-            #logging.debug('responding with error: bad measurement type')
             self.respond_error(ERR_SETUP)
             self.still_processing = False
             return
         self.measurement_type = measurement_type
         self.current_sequence_num = 1
-        #logging.debug('responding with OK')
         self.respond_ok('Ready')
 
     def handle_measurement(self, sequence_num, payload):
-        #logging.debug('sequence num {}'.format(sequence_num))
-        #logging.debug('payload {}'.format(payload))
         if int(sequence_num) > self.num_expected_messages:
-            #logging.debug('too many messages sent')
             self.respond_error(ERR_MEASURE)
             self.still_processing = False
             return
         if int(sequence_num) != self.current_sequence_num:
-            #3logging.debug('expected sequence number {}'
-            #              .format(self.current_sequence_num))
-            #logging.debug('sequence_num != self.current_seq_num: {}'
-             #             .format(sequence_num != self.current_sequence_num))
             self.respond_error(ERR_MEASURE)
             self.still_processing = False
             return
-        if len(payload) != self.message_size:
-            #logging.debug('expecting payload size {}'
-             #             .format(self.message_size))
-            #logging.debug('  got payload size {}'.format(len(payload)))
-            #logging.debug('len(payload) != self.message_size: {}'
-            #              .format(len(payload) != self.message_size))
+        if len(payload) != self.message_size:             
             self.respond_error(ERR_MEASURE)
             self.still_processing = False
             return
@@ -67,39 +52,31 @@ class MeasurementProtocolHandler(socketserver.StreamRequestHandler):
 
     def respond_ok(self, message):
         self.wfile.write('200 OK: {0}'.format(message).encode('UTF-8'))
-        #logging.debug('responded with OK')
 
     def respond_error(self, message):
         self.wfile.write('404 ERROR: {0}'.format(message).encode('UTF-8'))
 
     def handle(self):
-        # Other functions may set this to False in order to stop handling data
         # sent from the client.
         self.still_processing = True
         while self.still_processing:
-            #logging.debug('handling request')
             received_data = self.rfile.readline().strip()
-            # inputs come as `bytes` objects; for ease of use we decode to strings
             incoming = [x.decode() for x in received_data.split()]
-            #logging.debug('incoming {}'.format(incoming))
             if len(incoming) == 0:
                 self.respond_error(ERR_EMPTY)
                 break
             phase = incoming[0]
             if phase == 's':
-            #    logging.debug('  handling setup phase')
                 if len(incoming[1:]) != 4:
                     self.respond_error(ERR_SETUP)
                     break
                 self.handle_setup(*incoming[1:])
             elif phase == 'm':
-             #   logging.debug('  handling measurement phase')
                 if len(incoming[1:]) != 2:
                     self.respond_error(ERR_MEASURE)
                     break
                 self.handle_measurement(*incoming[1:])
             elif phase == 't':
-              #  logging.debug('  handling termination phase')
                 if len(incoming) > 1:
                     self.respond_error(ERR_TERM)
                 else:
@@ -116,10 +93,4 @@ def main(host, port):
     
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        #logging.critical('Must specify port on which to run the server.'
-        #logging.critical('    ./server.py <portnumber>')
-        sys.exit(1)
-    if len(sys.argv) >= 3 and sys.argv[2] == 'debug':
-        #logging.basicConfig(level=logging.DEBUG)
     main(socket.getfqdn(), int(sys.argv[1]))
